8e9c5f88a22c3e8a62712c0e8387d4856e2ad29a
tomchkk <tpm.moore@gmail.com>
2018-12-14 21:19:48 +0000

    Indentation fix

--- src/DependencyInjection/TomchkkTransliterableExtension.php
+++ src/DependencyInjection/TomchkkTransliterableExtension.php
@@ -29,7 +29,7 @@ class TomchkkTransliterableExtension extends Extension implements PrependExtensi
                     ]
                 ]
             ]
-         ]);
+        ]);
     }
 
     /**
